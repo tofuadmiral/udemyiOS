f85c071a2b0d3296b8a7842dfcebecabba2b5e55
tofuadmiral <fuad98ali@gmail.com>
2018-09-27 12:06:42 -0400

    initial commit

---
+++ hello world/hello world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hello world
+//
+//  Created by Ahmed Fuad Ali on 2018-09-27.
+//  Copyright © 2018 Ahmed Fuad Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
